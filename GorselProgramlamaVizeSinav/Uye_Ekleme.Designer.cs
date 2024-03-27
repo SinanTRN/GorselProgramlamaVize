cc193a52c4629a3f9c804ccf84b13c058f1fda6a
Sinan Turan <turansinan2004@gmail.com>
2024-03-27 17:23:45 +0300

    Üye ekleme formunda tasarımsal değişiklik yapıldı

--- GorselProgramlamaVizeSinav/Uye_Ekleme.Designer.cs
+++ GorselProgramlamaVizeSinav/Uye_Ekleme.Designer.cs
@@ -41,16 +41,12 @@
             label4 = new Label();
             txtTelefon = new TextBox();
             label3 = new Label();
-            groupBox3 = new GroupBox();
-            label5 = new Label();
-            txtParola = new TextBox();
             btnEkle = new Button();
             btnGuncelle = new Button();
             btnSil = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvUyeler).BeginInit();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
-            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // txtAra
@@ -58,7 +54,7 @@
             txtAra.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             txtAra.Location = new Point(22, 19);
             txtAra.Name = "txtAra";
-            txtAra.Size = new Size(292, 23);
+            txtAra.Size = new Size(393, 23);
             txtAra.TabIndex = 8;
             // 
             // label1
@@ -75,7 +71,7 @@
             // btnAra
             // 
             btnAra.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnAra.Location = new Point(320, 18);
+            btnAra.Location = new Point(421, 18);
             btnAra.Name = "btnAra";
             btnAra.Size = new Size(94, 23);
             btnAra.TabIndex = 9;
@@ -90,7 +86,7 @@
             dgvUyeler.GridColor = SystemColors.GradientActiveCaption;
             dgvUyeler.Location = new Point(22, 47);
             dgvUyeler.Name = "dgvUyeler";
-            dgvUyeler.Size = new Size(392, 391);
+            dgvUyeler.Size = new Size(529, 429);
             dgvUyeler.TabIndex = 3;
             dgvUyeler.CellContentClick += dgvUyeler_CellContentClick;
             // 
@@ -102,7 +98,7 @@
             groupBox1.Controls.Add(txtIsim);
             groupBox1.Controls.Add(label1);
             groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            groupBox1.Location = new Point(456, 47);
+            groupBox1.Location = new Point(575, 47);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(314, 100);
             groupBox1.TabIndex = 1;
@@ -112,9 +108,9 @@
             // txtSoyisim
             // 
             txtSoyisim.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            txtSoyisim.Location = new Point(152, 28);
+            txtSoyisim.Location = new Point(189, 28);
             txtSoyisim.Name = "txtSoyisim";
-            txtSoyisim.Size = new Size(140, 29);
+            txtSoyisim.Size = new Size(119, 29);
             txtSoyisim.TabIndex = 2;
             // 
             // label2
@@ -122,7 +118,7 @@
             label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label2.Location = new Point(152, 60);
+            label2.Location = new Point(189, 60);
             label2.Name = "label2";
             label2.Size = new Size(48, 15);
             label2.TabIndex = 1;
@@ -133,7 +129,7 @@
             txtIsim.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             txtIsim.Location = new Point(6, 28);
             txtIsim.Name = "txtIsim";
-            txtIsim.Size = new Size(140, 29);
+            txtIsim.Size = new Size(165, 29);
             txtIsim.TabIndex = 1;
             // 
             // groupBox2
@@ -144,7 +140,7 @@
             groupBox2.Controls.Add(txtTelefon);
             groupBox2.Controls.Add(label3);
             groupBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            groupBox2.Location = new Point(456, 153);
+            groupBox2.Location = new Point(575, 153);
             groupBox2.Name = "groupBox2";
             groupBox2.Size = new Size(314, 117);
             groupBox2.TabIndex = 2;
@@ -154,9 +150,9 @@
             // txtMail
             // 
             txtMail.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            txtMail.Location = new Point(65, 64);
+            txtMail.Location = new Point(107, 64);
             txtMail.Name = "txtMail";
-            txtMail.Size = new Size(221, 29);
+            txtMail.Size = new Size(201, 29);
             txtMail.TabIndex = 4;
             // 
             // label4
@@ -164,7 +160,7 @@
             label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label4.Location = new Point(14, 72);
+            label4.Location = new Point(6, 72);
             label4.Name = "label4";
             label4.Size = new Size(30, 15);
             label4.TabIndex = 0;
@@ -173,9 +169,9 @@
             // txtTelefon
             // 
             txtTelefon.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            txtTelefon.Location = new Point(65, 29);
+            txtTelefon.Location = new Point(107, 29);
             txtTelefon.Name = "txtTelefon";
-            txtTelefon.Size = new Size(221, 29);
+            txtTelefon.Size = new Size(201, 29);
             txtTelefon.TabIndex = 3;
             // 
             // label3
@@ -183,50 +179,18 @@
             label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label3.Location = new Point(14, 37);
+            label3.Location = new Point(6, 37);
             label3.Name = "label3";
             label3.Size = new Size(45, 15);
             label3.TabIndex = 0;
             label3.Text = "Telefon";
-            // 
-            // groupBox3
-            // 
-            groupBox3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            groupBox3.Controls.Add(label5);
-            groupBox3.Controls.Add(txtParola);
-            groupBox3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            groupBox3.Location = new Point(456, 276);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(314, 88);
-            groupBox3.TabIndex = 3;
-            groupBox3.TabStop = false;
-            groupBox3.Text = "🔐Parola";
-            // 
-            // label5
-            // 
-            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label5.Location = new Point(6, 35);
-            label5.Name = "label5";
-            label5.Size = new Size(40, 15);
-            label5.TabIndex = 0;
-            label5.Text = "Parola";
-            // 
-            // txtParola
-            // 
-            txtParola.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            txtParola.Location = new Point(57, 27);
-            txtParola.Name = "txtParola";
-            txtParola.Size = new Size(221, 29);
-            txtParola.TabIndex = 4;
             // 
             // btnEkle
             // 
             btnEkle.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnEkle.BackColor = Color.LightGreen;
             btnEkle.ForeColor = Color.Transparent;
-            btnEkle.Location = new Point(687, 382);
+            btnEkle.Location = new Point(806, 297);
             btnEkle.Name = "btnEkle";
             btnEkle.Size = new Size(83, 23);
             btnEkle.TabIndex = 7;
@@ -239,7 +203,7 @@
             btnGuncelle.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnGuncelle.BackColor = Color.Chocolate;
             btnGuncelle.ForeColor = Color.Transparent;
-            btnGuncelle.Location = new Point(581, 382);
+            btnGuncelle.Location = new Point(700, 297);
             btnGuncelle.Name = "btnGuncelle";
             btnGuncelle.Size = new Size(100, 23);
             btnGuncelle.TabIndex = 6;
@@ -251,7 +215,7 @@
             btnSil.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnSil.BackColor = Color.Red;
             btnSil.ForeColor = Color.Transparent;
-            btnSil.Location = new Point(456, 382);
+            btnSil.Location = new Point(575, 297);
             btnSil.Name = "btnSil";
             btnSil.Size = new Size(119, 23);
             btnSil.TabIndex = 5;
@@ -262,11 +226,10 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(901, 488);
             Controls.Add(btnSil);
             Controls.Add(btnGuncelle);
             Controls.Add(btnEkle);
-            Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Controls.Add(dgvUyeler);
@@ -279,8 +242,6 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
-            groupBox3.ResumeLayout(false);
-            groupBox3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -300,9 +261,6 @@
         private Label label4;
         private TextBox txtTelefon;
         private Label label3;
-        private GroupBox groupBox3;
-        private Label label5;
-        private TextBox txtParola;
         private Button btnEkle;
         private Button btnGuncelle;
         private Button btnSil;
